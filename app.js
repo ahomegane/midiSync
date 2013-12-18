9f1ff1e4f5bef5c1fb286538de5cb952ccf44eda
k-watanabe <macbookair@k-watanabe.local>
2013-12-18 15:30:11 +0900

    init

---
+++ app.js
@@ -0,0 +1,67 @@
+var http = require("http")
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs");
+var socketio = require("socket.io");
+
+var server = http.createServer(function(request, response) {
+     var Response = {
+        "200":function(file, filename){
+            var extname = path.extname(filename);
+            var header = {
+                "Access-Control-Allow-Origin":"*",
+                "Pragma": "no-cache",
+                "Cache-Control" : "no-cache"       
+            }
+            response.writeHead(200, header);
+            response.write(file, "binary");
+            response.end();
+        },
+        "404":function(){
+            response.writeHead(404, {"Content-Type": "text/plain"});
+            response.write("404 Not Found\n");
+            response.end();
+
+        },
+        "500":function(err){
+            response.writeHead(500, {"Content-Type": "text/plain"});
+            response.write(err + "\n");
+            response.end();
+
+        }
+    }
+    var uri = url.parse(request.url).pathname
+    , filename = path.join(process.cwd(), uri);
+
+    fs.exists(filename, function(exists){
+        console.log(filename+" "+exists);
+        if (!exists) { Response["404"](); return ; }
+        if (fs.statSync(filename).isDirectory()) { filename += '/index.html'; }
+
+        fs.readFile(filename, "binary", function(err, file){
+        if (err) { Response["500"](err); return ; }
+            Response["200"](file, filename);   
+        }); 
+
+    });
+}).listen(process.env.VMC_APP_PORT || 3000);
+
+var io = socketio.listen(server);
+
+io.sockets.on("connection", function (socket) {
+
+  // メッセージ送信（送信者にも送られる）
+  socket.on("C_to_S_message", function (data) {
+    io.sockets.emit("S_to_C_message", {value:data.value});
+  });
+
+  // ブロードキャスト（送信者以外の全員に送信）
+  socket.on("C_to_S_broadcast", function (data) {
+    socket.broadcast.emit("S_to_C_message", {value:data.value});
+  });
+
+  // 切断したときに送信
+  socket.on("disconnect", function () {
+//    io.sockets.emit("S_to_C_message", {value:"user disconnected"});
+  });
+});
